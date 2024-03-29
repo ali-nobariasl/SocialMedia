cea5f0b1339ccf82d2695c8ff7556ea4b8599214
Ali Nobariasl <freeeeboy.gh@gmail.com>
2024-03-29 21:48:50 +0300

    log in function

---
+++ socialproject/users/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(max_length=30, widget=forms.PasswordInput)
--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from .forms import LoginForm
 
-# Create your views here.
+
+
+def user_login(request):
+    if request.method == "POST":
+        myform = LoginForm(request.POST) 
+        if myform.is_valid():
+            username = myform.cleaned_data["username"]
+            password = myform.cleaned_data["password"]
+            user = authenticate(request,username=username, password=password)
+            if user is not None:
+                login(request,user)  
+                return HttpResponse("user logged in successfully")  
+            else:
+                return HttpResponse("Invalid username or password")  
+    else: 
+        myform = LoginForm() 
+        
+    context ={'form': myform}
+    return render(request,'users/login.html',context=context)
+
