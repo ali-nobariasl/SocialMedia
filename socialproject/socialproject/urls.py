03a8b1b124c4c891b8fe1a737dd6c7b507c2243d
Ali Nobariasl <freeeeboy.gh@gmail.com>
2024-03-31 22:39:16 +0300

    sa

--- socialproject/socialproject/urls.py
+++ socialproject/socialproject/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf.urls import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,4 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/',include('users.urls')),
-]
+
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
