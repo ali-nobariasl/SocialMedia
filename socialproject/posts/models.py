86a26ea0f508fe8b82f668ad4291dca8833fe0a9
Ali Nobariasl <freeeeboy.gh@gmail.com>
2024-04-01 21:33:34 +0300

    adding posts app

---
+++ socialproject/posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from users.models import Profile
+from django.conf import settings
+
+
+
+class PostModel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE)
+    content = models.TextField(blank=True)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='/posts/%y/%m/%d/')
+    slug = models.SlugField(max_length=200, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
