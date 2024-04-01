ef10cf7877bec21a7de9b30964fc4638204c4e6b
Ali Nobariasl <freeeeboy.gh@gmail.com>
2024-04-02 01:13:24 +0300

    adding slugify

--- socialproject/posts/models.py
+++ socialproject/posts/models.py
@@ -2,21 +2,22 @@ from typing import Iterable
 from django.db import models
 from users.models import Profile
 from django.conf import settings
-
+from django.utils.text import slugify
 
 
 class PostModel(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE)
     content = models.TextField(blank=True)
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='/posts/%y/%m/%d/')
+    image = models.ImageField(upload_to='posts/%y/%m/%d/')
     slug = models.SlugField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
     
-    def save(self):
-        
-        return super().save(force_insert, force_update, using, update_fields)
+    def save(self,*args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
     
