052a913e0b0f8e6b8360ab927c3536284c7b5293
Ali Nobariasl <freeeeboy.gh@gmail.com>
2024-04-17 01:31:51 +0300

    fixed issue

--- socialproject/socialproject/urls.py
+++ socialproject/socialproject/urls.py
@@ -4,12 +4,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from socialproject.settings import MEDIA_ROOT, MEDIA_URL
-
+from users import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('users/',include('users.urls')),
     path('posts/', include('posts.urls')),
+    path('',views.index, name='index')
 
 ] 
 
